d476209b28a17640eeb38046bd60b183d49ff9ce
Ginamkim <kimginam1995@gmail.com>
2020-04-09 13:46:15 +0900

    update 화면 구현 수정 x

--- build/resources/main/static/js/app/main.js
+++ build/resources/main/static/js/app/main.js
@@ -4,6 +4,9 @@ var main = {
         $('#btn-save').on('click', function () {
             _this.save();
         });
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
     },
     save : function () {
         var data = {
@@ -22,6 +25,27 @@ var main = {
             window.location.href = '/';
         }).fail(function (error) {
             alert(error);
+        });
+    },
+    update : function () {
+        var data = {
+            word: $('#word').val(),
+            meaning: $('#meaning').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
         });
     }
 
--- src/main/java/com/example/words/web/WebController.java
+++ src/main/java/com/example/words/web/WebController.java
@@ -1,10 +1,12 @@
 package com.example.words.web;
 
 import com.example.words.service.words.WordsService;
+import com.example.words.web.dto.WordsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -13,15 +15,20 @@ public class WebController {
     private final WordsService wordsService;
 
     @GetMapping("/")
-    public String main(Model model){
+    public String main(Model model) {
         model.addAttribute("words", wordsService.findAllDesc());
         return "main";
     }
 
-
-
     @GetMapping("/words/save")
-    public String wordsSave(){
+    public String wordsSave() {
         return "words-save";
+    }
+
+    @GetMapping("/words/update/{id}")
+    public String wordsUpdate(@PathVariable Long id, Model model){
+        WordsResponseDto dto = wordsService.findById(id);
+        model.addAttribute("post", dto);
+        return "words-update";
     }
 }
